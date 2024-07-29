73ebb1d3be76787685388beac5135852d1fa9f15
ryudongjae <rdj1014@naver.com>
2024-07-29 23:10:17 +0900

    # 1 - 1. 멀티 모듈 프로젝트 설정중

--- src/main/java/com/project/match/user/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.project.match.user.service;
-
-import org.springframework.transaction.TransactionManager;
-import org.springframework.transaction.TransactionStatus;
-
-public class UserService {
-}
