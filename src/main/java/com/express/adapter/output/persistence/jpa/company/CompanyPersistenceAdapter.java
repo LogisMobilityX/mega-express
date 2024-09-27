60865ce930d21d9fe7f79da526402ca28286ff78
임동혁 <idh1007@naver.com>
2024-09-27 17:06:15 +0900

    [ADD] 초기 company 관련 구조 구축 - output adpater (jpa repository)추가

---
+++ src/main/java/com/express/adapter/output/persistence/jpa/company/CompanyPersistenceAdapter.java
@@ -0,0 +1,28 @@
+package com.express.adapter.output.persistence.jpa.company;
+
+import com.express.application.port.output.company.CompanyProcessor;
+import com.express.application.port.output.company.CompanyReader;
+import com.express.domain.model.company.Company;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class CompanyPersistenceAdapter implements CompanyReader, CompanyProcessor {
+
+    private final CompanyJpaRepository companyJpaRepository;
+
+    @Override
+    public void modify(Company company) {
+
+    }
+
+    @Override
+    public void register(Company company) {
+
+    }
+
+    @Override
+    public Optional<Company> readById(String id) {
+        return Optional.empty();
+    }
+}
