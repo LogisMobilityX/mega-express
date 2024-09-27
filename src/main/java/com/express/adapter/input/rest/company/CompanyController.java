4b7639fa79da3c5ed3b253e732741ec208d8b9dc
임동혁 <idh1007@naver.com>
2024-09-27 16:59:29 +0900

    [ADD] 초기 company 관련 구조 구축 - input adpater (controller)추가

---
+++ src/main/java/com/express/adapter/input/rest/company/CompanyController.java
@@ -0,0 +1,24 @@
+package com.express.adapter.input.rest.company;
+
+import com.express.adapter.input.rest.common.CustomResponse;
+import com.express.adapter.input.rest.company.model.CompanyCreateModel;
+import com.express.application.port.input.company.CompanyUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api/v1/company")
+@RequiredArgsConstructor
+public class CompanyController {
+    private final CompanyUseCase companyUseCase;
+
+
+    @PostMapping
+    public ResponseEntity<CustomResponse> createCompany(@RequestBody CompanyCreateModel model){
+        companyUseCase.registerCompany(model) //model -> command mapper 추가
+
+        return null;
+    }
+}
