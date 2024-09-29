43159febc2d2174dc21f02d33688b318adf06b11
ryudongjae <rdj1014@naver.com>
2024-09-29 20:34:59 +0900

    [REFACTOR] Handler class naming update

--- src/main/java/com/express/domain/model/user/event/handler/UserEventBusHandler.java
+++ src/main/java/com/express/domain/model/user/event/handler/UserEventBusHandler.java
@@ -1,10 +1,8 @@
 package com.express.domain.model.user.event.handler;
 
 
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -12,13 +10,13 @@ import org.springframework.transaction.event.TransactionalEventListener;
 @Component
 @Slf4j
 @RequiredArgsConstructor
-public class EventBusUserHandler {
+public class UserEventBusHandler {
     /**
      * 유저 관련 이벤트를 처리
      *
      */
 
-    //save User 이후 처리 될 이벤트 
+    //save User 이후 처리 될 이벤트
     @Async
     @TransactionalEventListener
     public void Handle(){
